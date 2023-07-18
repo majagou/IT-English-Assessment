b07f91d224da036d94177efd1b47d367bfa60082
majagou <56938217+majagou@users.noreply.github.com>
2023-07-18 13:26:07 +0200

    Annotation

--- js/listening.js
+++ js/listening.js
@@ -1,5 +1,7 @@
+//Examples for tests
 //let article = "This is the first part ChatGPT. This is the second part Machine Learning. This is the third part Tech. This is the fourth part Chatbot. This is the fifth part AI. This is the sixth part.";
 //let keyWords = ["ChatGPT", "Machine", "Tech", "Chatbot", "AI"];
+
 const articles = Array.from(document.getElementsByClassName("article"));
 const scoreText = document.getElementById("score");
 const audioDiv = document.getElementById("listen-audio");
@@ -8,9 +10,9 @@ let article = "";
 let keyWords =[];
 let currentQuestion = {};
 let score = 0;
-let questionCounter = 0;
 let availableQuestions = [];
 
+//Fetch the file and prepare the question list
 fetch("listen.json")
   .then((res) => {
     return res.json();
@@ -24,15 +26,15 @@ fetch("listen.json")
     console.error(err);
   });
 
+//Main function to start and set up avialable questions
 setupFillInTheBlanks = () => {
-  questionCounter = 0;
   availableQuestions = [...questions];
 
   getNewQuestion();
 };
-  
+
+//Ramdonly choose a article and set up the blanks and answers
 getNewQuestion = () => {
-  questionCounter++;
   let audioHTML = "";
 
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
@@ -51,40 +53,41 @@ getNewQuestion = () => {
   });
 }
 
+// Convert the article to lowercase to make the search case-insensitive
+// Iterate through the article to find occurrences of keywords
+// If the keyword is found, slice the article from the start position to the keyword position
+// and update the start position for the next iteration
 function cutArticleWithKeywords(article, keywords) {
-  // Convert the article to lowercase to make the search case-insensitive
-  const articleLower = article.toLowerCase();
 
-  // Initialize an array to store the cut segments
+  const articleLower = article.toLowerCase();
   const cutSegments = [];
 
-  // Start position for slicing the article
   let startPos = 0;
 
-  // Iterate through the article to find occurrences of keywords
   for (const keyword of keywords) {
     const keywordLower = keyword.toLowerCase();
     const keywordPos = articleLower.indexOf(keywordLower, startPos);
 
-    // If the keyword is found, slice the article from the start position to the keyword position
-    // and update the start position for the next iteration
     if (keywordPos !== -1) {
       cutSegments.push(article.substring(startPos, keywordPos).trim());
-      startPos = keywordPos + keyword.length; // Update start position to exclude the keyword
+      // Update start position to exclude the keyword
+      startPos = keywordPos + keyword.length; 
     }
   }
 
-  // After processing all keywords, add the remaining part of the article as the last segment
   cutSegments.push(article.substring(startPos).trim());
 
   return cutSegments;
 }
 
+//Jump to to next test page and pass the score
 function goToNextPage() {
   window.location.href = `match.html?score=${score}`;
 }
 
-//check if the answers are right
+//Check if each answer is correct
+//Ruturn all the rusults and insert the rusult HTML
+//Disappear the submit button and show the next page button to continue the test
 function checkAnswers() {
   const blanks = document.querySelectorAll(".blank");
   const resultsDiv = document.getElementById("results");
